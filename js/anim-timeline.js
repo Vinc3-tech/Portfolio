121d6d097fd176fee220770005fb1d19e43301d4
Vinc3-tech <vincenzov.mob@gmail.com>
2025-10-29 19:50:47 +0100

    Add education timeline section with animation
    Introduced a new education timeline section in home.html, styled with new CSS classes in stile.css. Added anim-timeline.js to dynamically assign grid positions and fonts to timeline entries, enhancing the visual presentation and interactivity of the education history.

---
+++ js/anim-timeline.js
@@ -0,0 +1,29 @@
+//seleziona le annate
+const annate = document.querySelectorAll('.annata');
+
+if (annate) {
+    //assegno le righe e colonne alle annate
+    annate.forEach((element, i) => {
+        if (element) {
+            element.style.gridColumn = (i % 2 === 0) ? 2 : 1; //se l'indice è pari allora la colonna è la seconda, altrimnti è la prima
+            element.style.textAlign = (i % 2 === 0) ? "left" : "right";
+            if (i%2 === 0) {
+                element.style.setProperty('--pseudo-positionX', '0%');
+                element.style.setProperty('--pseudo-translateX', '-175%');
+            } else {
+                element.style.setProperty('--pseudo-positionX', '100%');
+                element.style.setProperty('--pseudo-translateX', '75%');
+            }
+            element.style.gridRow = i+1;    //incrementa la riga ogni elemento
+        }
+    });
+}
+
+//assegno i font
+document.querySelectorAll('.titAnno').forEach(element =>{
+    element.classList.add('inter');
+    element.style.fontWeight = "600"
+});
+document.querySelectorAll('.descAnno').forEach(element =>{
+    element.classList.add('instrument-sans');
+});
